d90b5b22f8ebd6e734a1be25a7d5bd17b247c203
Cy Lim <cylim@CYs-Macbook-2015.local>
2016-05-20 16:10:40 +1000

    Update: Firebase 2 to 3

--- UOWConsult/AppDelegate.swift
+++ UOWConsult/AppDelegate.swift
@@ -8,17 +8,17 @@
 
 import UIKit
 import CoreData
-import Firebase
+import FirebaseAnalytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
 
-
 	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 		// Override point for customization after application launch.
-        
+		FIRApp.configure()
+		
 		return true
 	}
 
